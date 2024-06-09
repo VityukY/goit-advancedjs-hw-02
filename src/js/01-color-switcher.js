d3643fe0aa1ca844ebb0f88f250c463f67d61097
Yuriy Yuriy <you.thunderstruck@gmail.com>
2024-06-09 18:42:54 +0300

    add task 3 adn iztoats

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,20 @@
+function getRandomHexColor() {
+
+   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+   }
+
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+const bodyRef = document.querySelector('body')
+let timerId = null;
+
+startButton.addEventListener('click', ()=> {
+   bodyRef.style.backgroundColor =getRandomHexColor()
+   timerId = setInterval(() => bodyRef.style.backgroundColor =getRandomHexColor(),  1000);
+   startButton.disabled = true;
+ })
+
+ stopButton.addEventListener('click', ()=> {
+   clearInterval(timerId);
+   startButton.disabled = false;
+ })
---
+++ src/js/03-promises.js
@@ -0,0 +1,52 @@
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const formRef = document.querySelector('.form')
+
+formRef.addEventListener('submit', (e)=> {
+  e.preventDefault();
+
+
+  const delay = Number(e.target.delay.value) 
+  const step = Number (e.target.step.value)
+  const amount =  Number (e.target.amount.value)
+
+  for (let i = 1; i<= amount; i++) {
+    const currentDelay = delay + ((i-1) * step);
+    createPromise(i, currentDelay)
+      .then(({ position, delay }) => {
+        iziToast.show({
+          title: 'Promise',
+          backgroundColor: 'green',
+          messageColor: 'white',
+          message: `✅ Fulfilled promise ${position} in ${delay}ms`
+      });
+      })
+      .catch(({ position, delay }) => {
+        iziToast.show({
+          title: 'Promise',
+          backgroundColor: 'red',
+          messageColor: 'white',
+          message: `❌ Rejected promise ${position} in ${delay}ms`
+      });
+
+      });  
+    }
+  }
+)
+
+
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
